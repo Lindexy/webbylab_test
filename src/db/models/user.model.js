4ea0917d7616432fbba0a3dac84ded1f08a40284
Olexandr Stelm <novus7827@gmail.com>
2024-04-03 16:03:58 +0300

    create user model

---
+++ src/db/models/user.model.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define("user", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+};
